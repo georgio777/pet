9acf6e24a59f9fffe1f0acb9308bc93f61c6a72c
George <wallzy@mail.ru>
2024-11-22 22:45:45 +0300

    url fix

--- js/cart.js
+++ js/cart.js
@@ -13,11 +13,11 @@ closeCartIcon.addEventListener('click', ()=>{
   cartModal.classList.add('none')
 })
 
-window.addEventListener('click', (e)=>{
-  if (e.target == cartModal) {
-    cartModal.classList.add('none')
-  }
-})
+// window.addEventListener('click', (e)=>{
+//   if (e.target == cartModal) {
+//     cartModal.classList.add('none')
+//   }
+// })
 
 // добавляет красную бляшку когда корзина не пуста
 
@@ -72,8 +72,17 @@ function cartCreator() {
   
   
   cartContainer.appendChild(itemContainer)
+
+  console.log(itemFromLocalStorage.imgURL);
+  console.log(window.location.pathname)
   
-  document.querySelector('.mycart__item').innerHTML = `<img src="${itemFromLocalStorage.imgURL}" alt="" class="mycart__img"> 
+  // фикс ссылки для индекс хтмл
+  let watchUrl = itemFromLocalStorage.imgURL
+  if (window.location.pathname == "/index.html") {
+    watchUrl = itemFromLocalStorage.indexImgUrl;
+  } 
+  
+  document.querySelector('.mycart__item').innerHTML = `<img src="${watchUrl}" alt="" class="mycart__img"> 
           <div class="mycart__item--info--wrapper">
             <p class="mycart__item--name">${itemFromLocalStorage.name}</p>
             <p class="mycart__item--totalprice">${itemFromLocalStorage.currentPrice}</p>
